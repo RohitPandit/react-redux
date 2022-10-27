cf899dec31435f1f731f6491c707ce90b150f8f0
rohit.pandit <rohit.pandit@inducchain.com>
2022-10-27 17:10:33 +0530

    Successfully Integrate Simple Redux Increment and Decrement number by 1.

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const incNumber = () => {
+    return { 
+        type: 'INCREMENT_NUMBER' 
+    }
+}
+
+export const decNumber = () => {
+    return { 
+        type: 'DECREMENT_NUMBER' 
+    }
+}
